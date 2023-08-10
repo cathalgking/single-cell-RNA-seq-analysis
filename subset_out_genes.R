1d11ea168001806a51ac8bc1bb383d9e97600c08
Cathal King <32261323+cathalgking@users.noreply.github.com>
2023-08-10 18:36:45 +0930

    Add files via upload

---
+++ subset_out_genes.R
@@ -0,0 +1,13 @@
+## subset single-cell object to only contain certain genes
+
+# define genes
+genes <- c("CCL14", "MRC1", "TGM2", "CXADR", "VWF", "SEMA3A", "NAMPT", "A2M", "CD59", "HSPG2")
+
+# first check if genes are in the rownames
+genes %in% rownames(sce)
+
+# Seurat object
+seu_filt <- seu[genes,]
+
+# SCE object
+sce_filt <- sce[genes,]
