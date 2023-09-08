dc26c42e41420a8d8ba592bd104a19eaa329c661
Cathal King <32261323+cathalgking@users.noreply.github.com>
2023-09-08 15:52:09 +0930

    Update cellranger_many.sh
    use a data array to run through cellranger or any tool

--- cellranger_many.sh
+++ cellranger_many.sh
@@ -6,23 +6,29 @@
 #SBATCH --time=0-12:00:00
 #SBATCH --output=01-Cellranger_Count_log.slurm.%a.stdout
 #SBATCH --array=1-10
-#SBATCH --mail-user=cathal.king@sahmri.com
+#SBATCH --mail-user=EMAIL
 #SBATCH --mail-type=END,FAIL
 #SBATCH --job-name="CRC"
 
+# declare the name of the array
 declare -A DATA
 
-#DATA[1]='21-01749'
-#DATA[2]='21-01750'
-#DATA[3]='21-01751'
-#DATA[4]='21-01756'
-#DATA[5]='21-01757'
-#DATA[6]='21-01758'
-#DATA[7]='21-01872'
-#DATA[8]='21-01873'
-#DATA[9]='21-01874'
-#DATA[10]='21-01875'
+# each DATA below corresponds to the first part of the FASTQ file name for example "21-01877_S1_L001_R2_001.fastq.gz"
+# cellranger requires Illumina format FASTQ file names
+# paired-end sequencing
+
+DATA[1]='21-01749'
+DATA[2]='21-01750'
+DATA[3]='21-01751'
+DATA[4]='21-01756'
+DATA[5]='21-01757'
+DATA[6]='21-01758'
+DATA[7]='21-01872'
+DATA[8]='21-01873'
+DATA[9]='21-01874'
+DATA[10]='21-01875'
 DATA[1]='21-01876'
 DATA[2]='21-01877'
 
-cellranger count --id=${DATA[$SLURM_ARRAY_TASK_ID]} --fastqs=./ --sample=${DATA[$SLURM_ARRAY_TASK_ID]} --transcriptome=/homes/cathal.king/References/refdata-gex-mm10-2020-A --no-bam
+# add commands as needed
+cellranger count --id=${DATA[$SLURM_ARRAY_TASK_ID]} --fastqs=./ --sample=${DATA[$SLURM_ARRAY_TASK_ID]} --transcriptome=/PATH/refdata-gex-mm10-2020-A --no-bam
