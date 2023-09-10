5e6327cbc76eac1b32347a1ee06e4d08195b2564
Cathal King <32261323+cathalgking@users.noreply.github.com>
2023-09-10 16:56:04 +0930

    Update read_in_10x_SPE.R

--- read_in_10x_SPE.R
+++ read_in_10x_SPE.R
@@ -2,7 +2,7 @@
 library(SpatialExperiment)
 library(ggspavis)
 # path must contain a 10x "/outs/" folder
-sample <- file.path("/Users/cathal.king/Documents/SAHMRI/Visium_Analysis/Outs/C1_outs/outs/")
+sample <- file.path("/PATH/outs/")
 # read in
 C1_sce <- read10xVisium(samples = sample, type = "sparse", images = "lowres")
 C1_sce
