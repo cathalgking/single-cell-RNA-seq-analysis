fbb6e343d0e8ab82bc4962983dbaa8ef1fd51b00
Cathal King <32261323+cathalgking@users.noreply.github.com>
2023-11-07 12:39:38 +1030

    Update read_in_10x_SPE.R
    ck

--- read_in_10x_SPE.R
+++ read_in_10x_SPE.R
@@ -1,4 +1,4 @@
-#
+### to a SPE (SpatialExperiment) object
 library(SpatialExperiment)
 library(ggspavis)
 # path must contain a 10x "/outs/" folder
@@ -8,3 +8,21 @@ C1_sce <- read10xVisium(samples = sample, type = "sparse", images = "lowres")
 C1_sce
 # View image in SPE object
 plotVisium(C1_sce)
+
+
+### to a Seurat object
+# Load a 10x Genomics Visium Spatial Experiment into a Seurat object
+seu_pros_data <- Seurat::Load10X_Spatial(
+  # The directory contains the read count matrix H5 file and the image data in a subdirectory called `spatial`. 
+  data.dir = "/PATH/outs", 
+  filename = "filtered_feature_bc_matrix.h5",
+  assay = "Spatial", # specify name of the initial assay
+  slice = "slice1", # specify name of the stored image
+  filter.matrix = TRUE, 
+  to.upper = FALSE
+)
+#
+seu_pros_data
+# plot
+SpatialPlot(object = seu_pros_data)
+
