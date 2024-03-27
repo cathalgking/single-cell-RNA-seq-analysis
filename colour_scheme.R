eed4a205a4fb1726bc884a92c92fd45b306983aa
Cathal King <32261323+cathalgking@users.noreply.github.com>
2024-03-28 09:15:36 +1030

    Create colour_scheme.R
    ck

---
+++ colour_scheme.R
@@ -0,0 +1,34 @@
+# A useful coloour scheme for R plots such as UMAP
+
+
+# this is probably the best colour scheme
+color.panel = c("#73CA6F", 
+                "#AF75CE", 
+                "#C5916F", 
+                "#ABC0D4", 
+                "#ECEAC5", 
+                "#C49CA4", 
+                "#C66FA0", 
+                "#706EC4", 
+                "#DDCDE3", 
+                "#7395C3", 
+                "#ACA1DC", 
+                "#E5C8BB", 
+                "#C96C69", 
+                "#FFFF66", 
+                "#99ff99", 
+                "#C1AE76", 
+                "#C4E6E3", 
+                "#C3D37B", 
+                "#BCE9BD", 
+                "#7ABB9B",
+                "#583612",
+                "#1940d3",
+                "grey70",
+                "khaki1",
+                "lightyellow",
+                "orchid4",
+                "lavender",
+                "tan3",
+                "snow3")
+##
